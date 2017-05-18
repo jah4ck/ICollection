41f01335f33a00f2fbcac589a6232b794b000b36
jah4ck <johann.simon.web@gmail.com>
2017-05-18 13:49:19 +0200

    first commit

---
+++ Dictionary/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Schedule> ScriptScheduleListe = new List<Schedule>();
+            ICollection<Schedule> ScriptScheduleCollection = ScriptScheduleListe;
+            ScriptScheduleCollection.Add(new Schedule("titi", "HEURE", 1, DateTime.Now));
+            ScriptScheduleCollection.Add(new Schedule("log", "MINUTE", 4, DateTime.Now));
+
+            foreach (Schedule script in ScriptScheduleCollection)
+            {
+                Console.WriteLine(script.namescript+";"+script.dernierExec.ToString());
+                
+            }
+            Thread.Sleep(2000);
+            //string periode = from namescript in ScriptScheduleCollection where namescript = "titi" select namescript;
+            ScriptScheduleCollection.Where(c => c.namescript == "titi").First().dernierExec=DateTime.Now;
+            Console.WriteLine(ScriptScheduleCollection.Where(c => c.namescript == "titi").First().dernierExec.ToString());
+            
+
+        }
+    }
+}
---
+++ Dictionary/Schedule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dictionary
+{
+    public class Schedule
+    {
+        public string namescript { get; set; }
+        public string periode { get; set; }
+        public int interval {get;set;}
+        public DateTime dernierExec { get; set; }
+        public Schedule(string _namescript, string _periode,int _interval,DateTime _dernierLancement)
+        {
+            namescript = _namescript;
+            periode = _periode;
+            interval = _interval;
+            dernierExec = _dernierLancement;
+        }
+        public void SetDernierExec(DateTime date)
+        {
+            dernierExec = date;
+        }
+    }
+}
